5653cba013f1f4718d847c0f0ae179262075be4b
bitjian <1183048003@qq.com>
2023-12-12 14:18:13 +0800

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import {WINSTON_LOGGER_TOKEN} from './winston/winston.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    cors:true
+  });
   app.setGlobalPrefix('/api')
   app.useLogger(app.get(WINSTON_LOGGER_TOKEN));
-  app.enableCors({
-    origin: 'https://manage.bitjian.cn/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
-  });
   await app.listen(3000);
 }
 bootstrap();
